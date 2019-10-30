346ad5676b3019acf668a65628c043f1d22e1c97
Sandra <samygomez150616@gmail.com>
2019-10-30 13:55:42 -0500

    Dist a publicar
    Dist

---
+++ dist/precache-manifest.d8d11fc4b65b674d6a672b1c69cc59bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0056127b8618fd5621ddb9d29c7fb6b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec007dce82437ee2e1e7",
+    "url": "/static/js/2.ae285c86.chunk.js"
+  },
+  {
+    "revision": "7be9524e61ced235a5b6",
+    "url": "/static/js/main.fd4c26af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "748b78e2e46fb67d3e8d9607fb42e986",
+    "url": "/static/media/LectoSord.748b78e2.png"
+  },
+  {
+    "revision": "2f25ae8e324eccbfa5b4c240000b98cd",
+    "url": "/static/media/Muybien.2f25ae8e.jpg"
+  },
+  {
+    "revision": "0469271bf7f6316b27467024ea2636e5",
+    "url": "/static/media/feliz.0469271b.png"
+  },
+  {
+    "revision": "8c0b4eb47f6e6c943fa8f2a24792f01d",
+    "url": "/static/media/triste.8c0b4eb4.png"
+  }
+]);
